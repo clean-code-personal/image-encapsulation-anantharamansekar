a6edc0760566bc66ec19d997c3d48b5f920e666c
Sekar, Anantharaman <Anantharaman.Sekar@philips.com>
2024-04-15 18:46:32 +0530

    Updated code.

--- image-encapsulate.cpp
+++ image-encapsulate.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include "brightener.h"
 
-int main() {
-	auto image = std::make_unique<Image>(512, 512);
+int main() 
+{
+	auto image = std::make_shared<Image>(512, 512);
 	std::cout << "Brightening a 512 x 512 image\n";
-	ImageBrightener brightener(std::move(image));
-	if (brightener.ValidateImage()) {
+	std::cout << image.use_count() << std::endl;
+
+	if (image->ValidateImage())
+	{
+		ImageBrightener brightener(image);
+		std::cout << "After Image brightener instantiation" << image.use_count() << std::endl;
 		int attenuatedCount = brightener.BrightenWholeImage();
 		std::cout << "Attenuated " << attenuatedCount << " pixels\n";
-		// Error... cannot access image here!
 		std::cout << "... in an image of: " << image->m_rows << " x " << image->m_columns << "\n";
 		return 0;
 	}
-	else {
+	else 
+	{
 		std::cout << "Not a valid image... did nothing\n";
 		return 1;
-	}
+	}	
 }
